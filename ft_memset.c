c8ab35db84db7d782d1cc84138f35bd3d3df33b4
Benjamin Gales <bgales@c2r7p4.42nice.fr>
2021-10-19 18:53:06 +0200

    he

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: bgales <bgales@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/19 18:03:18 by bgales            #+#    #+#             */
-/*   Updated: 2021/10/19 18:14:11 by bgales           ###   ########.fr       */
+/*   Updated: 2021/10/19 18:51:24 by bgales           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,9 @@ void* ft_memset(void* b, int c, size_t len)
 }
 int main()
 {
-    char dest[10]; 
-    printf("%s", memset(dest, 0, 0));
+    char dest[12] = "saluuuuuut";
+    
+    printf("%s\n", dest);
+    printf("%s\n", memset(dest + 3, '.', 1 * sizeof(char)));
+    printf("%s\n", dest);
 }
